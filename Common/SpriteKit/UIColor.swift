8ef37fcea347bdc882d5f5f56bd2033046e7d14a
Eric Russell <cire1776@gmail.com>
2022-07-26 07:16:08 -0400

    Add typealias to shim UIColor on macOS.

---
+++ Common/SpriteKit/UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  Common
+//
+//  Created by Eric Russell on 7/26/22.
+//
+
+import Foundation
+
+#if os(macOS)
+    import SpriteKit
+
+    public typealias UIColor = SKColor
+#endif
